1a619d39785bbb2b922ad220641a376dd8974175
Josh Glantz-Hucks <jahteo@gmail.com>
2021-01-13 22:10:20 -0800

    begun considering shape of data & displayed it.

--- src/scenes/Characters/Characters.js
+++ src/scenes/Characters/Characters.js
@@ -1,10 +1,29 @@
 import React from "react";
 import Page from "../../shared/components/Page/Page";
 
+const pcDetails = [
+  { name: "Snips", player: "Airiel", race: "Lizardfolk??", class: "", hp: "", ac: "", level: "" },
+  { name: "Kaidok", player: "Kyle", race: "", class: "", hp: "", ac: "", level: "" },
+];
+
 function Characters() {
   return (
     <Page title="PC's">
       Jello from the Characters page of whosits
+      {pcDetails.map(pc => {
+        return (
+          <>
+            Name: {pc.name} <br />
+            Player: {pc.player} <br />
+            race: {pc.race} <br />
+            class: {pc.class} <br />
+            hp: {pc.hp} <br />
+            ac: {pc.ac} <br />
+            level: {pc.level}
+            <br /><br />
+          </>
+        );
+      })}
     </Page>
   );
 }
